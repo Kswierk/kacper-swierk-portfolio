745176ffe7aaa02cb1a147c58501d025e517b167
Kswierk <kacper.swierk@gmail.com>
2020-12-07 10:10:11 +0100

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react"
-import Hero from "../components/hero"
-import Layout from "../components/layout"
-import Navbar from "../components/Navbar/navbar"
-import Sidebars from "../components/sidebars"
-import About from "../components/about"
-import Technologies from "../components/Technologies/technologies"
-import Work from "../components/Work/work"
-import Contact from "../components/contact"
-import Footer from "../components/footer"
+import Hero from "../components/Hero"
+import Layout from "../components/Layout"
+import Navbar from "../components/Navbar/Navbar"
+import Sidebars from "../components/Sidebars"
+import About from "../components/About"
+import Technologies from "../components/Technologies/Technologies"
+import Work from "../components/Work/Work"
+import Contact from "../components/Contact"
+import Footer from "../components/Footer"
 
 const IndexPage = () => {
   return (
